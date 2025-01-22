6481ddf3a50a78d81e873ad83f078fcdd560620d
lontraclick <comicplaycraft@gmail.com>
2025-01-21 23:02:09 -0300

    Update lontraads-clickident.js

--- lontraads-clickident.js
+++ lontraads-clickident.js
@@ -7,29 +7,50 @@
         return new URLSearchParams(window.location.search);
     }
 
+    function replaceSpacesAndDashes(inputString) {
+        return inputString.replace(/ /g, '_s_').replace(/-/g, '_d_').replace(/\//g, '');
+    }
+
     function getClickId() {
         var urlParams = getAllUrlParams();
-        return urlParams.get('gclid') || urlParams.get('wbraid') || urlParams.get('msclkid') || urlParams.get('fbclid') || '';
+        var clickId = urlParams.get('gclid') || urlParams.get('wbraid') || urlParams.get('msclkid') || urlParams.get('fbclid') || '';
+        
+        if (clickId) {
+            localStorage.setItem('clickId', clickId);
+        } else {
+            clickId = localStorage.getItem('clickId') || '';
+        }
+
+        return clickId;
     }
 
     function updateLinks() {
         var urlParams = getAllUrlParams();
         var clickId = getClickId();
+        var modifiedClickId = replaceSpacesAndDashes(clickId);
         var links = document.getElementsByTagName('a');
+
+        if (urlParams.has('tid')) {
+            var originalTid = urlParams.get('tid');
+            var replacedTid = replaceSpacesAndDashes(originalTid);
+            urlParams.set('tid', replacedTid);
+        }
 
         for (var i = 0; i < links.length; i++) {
             var link = links[i];
             var url = new URL(link.href);
-            
+            var linkHref = url.href.split('#')[0];
+            var anchorHash = url.hash;
+
+            if (modifiedClickId) {
+                linkHref = linkHref.replace('[sclid]', modifiedClickId).replace('%5Bsclid%5D', modifiedClickId);
+            }
+
             urlParams.forEach(function(value, key) {
                 url.searchParams.set(key, value);
             });
 
-            if (clickId) {
-                url.searchParams.set('sclid', clickId);
-            }
-
-            link.href = url.toString();
+            link.href = linkHref + '?' + url.searchParams.toString() + anchorHash;
         }
     }
 
@@ -46,33 +67,30 @@
         console.log("Current Domain:", currentDomain, "Plugin Domain:", pluginDomain);
 
         if (currentDomain !== pluginDomain) {
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', originUrl + '/wp-admin/admin-ajax.php', true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        console.log('Dados do visitante enviados com sucesso');
-                    } else {
-                        console.error('Erro ao enviar dados do visitante');
-                    }
-                }
+            var clickId = getClickId();
+            var data = {
+                action: 'lontraads_record_visit',
+                domain: currentDomain,
+                click_id: clickId
             };
 
-            var urlParams = getAllUrlParams();
-            var clickId = getClickId();
-            var data = 'action=lontraads_record_visit' +
-                       '&domain=' + encodeURIComponent(currentDomain) +
-                       '&click_id=' + encodeURIComponent(clickId);
-
-            urlParams.forEach(function(value, key) {
+            getAllUrlParams().forEach(function(value, key) {
                 if (!['gclid', 'wbraid', 'msclkid', 'fbclid'].includes(key)) {
-                    data += '&' + encodeURIComponent(key) + '=' + encodeURIComponent(value);
+                    data[key] = value;
                 }
             });
 
-            xhr.send(data);
+            fetch(originUrl + '/wp-admin/admin-ajax.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: new URLSearchParams(data)
+            })
+            .then(response => response.json())
+            .then(data => console.log('Dados do visitante enviados com sucesso:', data))
+            .catch(error => console.error('Erro ao enviar dados do visitante:', error));
+
             console.log("Dados enviados:", data);
         }
     }
@@ -83,12 +101,12 @@
         updateLinks();
     }
 
-    if (document.readyState === 'loading') {
-        console.log("DOM ainda carregando, adicionando evento listener");
-        document.addEventListener('DOMContentLoaded', init);
-    } else {
+    if (document.readyState !== 'loading') {
         console.log("DOM já carregado, executando funções imediatamente");
         init();
+    } else {
+        console.log("DOM ainda carregando, adicionando evento listener");
+        document.addEventListener('DOMContentLoaded', init);
     }
 
     console.log("Script concluído");
